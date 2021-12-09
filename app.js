7c350a0eb9772ccbc7c200a03ed30c40d6814424
Yong Quan Tan <yongquan26@gmail.com>
2021-12-08 20:10:58 -0500

    removed uri from .env and declare in app.js

--- app.js
+++ app.js
@@ -2,12 +2,20 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const path = require('path')
+let uri = "mongodb+srv://hermit46:Water1sl!fe@cluster0.hlswi.mongodb.net/Stocks?retryWrites=true&w=majority"
+const options = {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  };
+
 
 //set up our express app
 const app = express()
  
  // connect to mongoDB
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+mongoose.connect(uri, options)
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', (error) => console.log("Connected to Database."))
@@ -26,10 +34,6 @@ app.get('/search', function(req,res) {
     if (req.query['query'] == "") {
         res.send("Empty search! <p> <a href ='/'> Home Page </a>");
         return;
-        // res.write("Empty search!", encoding='utf8')
-        // document.getElementById("error").setAttribute("display", "block")
-        // document.getElementById("error").setAttribute("visibility", "visible")
-        // document.getElementById("error").innerHTML("Empty Search!")
     }
     // When searching for symbol
     else if (req.query['type'] == 'symbol') {
